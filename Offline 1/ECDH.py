f00f5923eee6a17540ecaa7e33e4dee1a3a9bebf
Rakib Abdullah <rakibabdullah@Rakibs-MacBook-Air.local>
2023-12-04 01:42:13 +0600

    independent implementation of ECDH done

--- Offline 1/ECDH.py
+++ Offline 1/ECDH.py
@@ -52,18 +52,42 @@ def double_and_add(G, k, p):
      
     return target_point
 
+def calculateKey(key_size):
+    #key generation
+    K_a=random.getrandbits(key_size) #privateKey of alice
+    K_b=random.getrandbits(key_size) #privateKey of bob
 
-#key generation
-K_a=random.getrandbits(128) #privateKey of alice
-K_b=random.getrandbits(128) #privateKey of bob
+    time0=time.time()
+    A=double_and_add(G,K_a,p) #publicKey of alice
+    time1=time.time()
+    B=double_and_add(G,K_b,p) #publickey of Bob
+    time2=time.time()
+    RAlice=double_and_add(B,K_a,p)
+    RBob=double_and_add(A,K_b,p)
+    time3=time.time()
 
-A=double_and_add(G,K_a,p) #publicKey of alice
-B=double_and_add(G,K_b,p) #publickey of Bob
+    R=RAlice
+    return (time1-time0),(time2-time1),(time3-time2)
 
-RAlice=double_and_add(B,K_a,p)
-RBob=double_and_add(A,K_b,p)
-R=RAlice
-print(R)
-print("and")
-print(RBob)
+key_sizes = [128, 192, 256]
+num_iterations=5
+for key_size in key_sizes:
+    total_time_1 = 0
+    total_time_2 = 0
+    total_time_3 = 0
 
+    for _ in range(num_iterations):
+        time_1, time_2, time_3 = calculateKey(key_size)
+        total_time_1 += time_1
+        total_time_2 += time_2
+        total_time_3 += time_3
+
+    avg_time_A = total_time_1 / num_iterations
+    avg_time_B = total_time_2 / num_iterations
+    avg_time_shared_key = total_time_3 / num_iterations
+
+    print(f"Key Size: {key_size} bits")
+    print(f"Average Time for A: {avg_time_A} seconds")
+    print(f"Average Time for B: {avg_time_B} seconds")
+    print(f"Average Time for Shared Key: {avg_time_shared_key} seconds")
+    print()
